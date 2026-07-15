9cedcfe358eb612ab0342d7ca2fd142c1eec16de
Shishir <shishirnemo1@gmail.com>
2026-07-15 21:00:44 +0530

    feat(backend): implement admin and user history APIs

--- BACKEND/Routes/resume.js
+++ BACKEND/Routes/resume.js
@@ -4,5 +4,7 @@ const resumeController = require("../Controllers/resume")
 const { upload } = require("../utils/multer")
 
 router.post("/addResume", upload.single("resume"), resumeController.addResume)
+router.get('/get/:user', resumeController.getAllResumesForUser)
+router.get('/get', resumeController.getResumeForAdmin)
 
 module.exports = router
